5405ab7b8970fe723aff776abe934a7f6dea853f
Eric Emery <eric.emery@bottlerocketstudios.com>
2023-12-06 14:08:15 -0600

    ASAA-121 - Migrate library to KMP

---
+++ kmp-launchpad-utils/build.gradle.kts
@@ -0,0 +1,96 @@
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.kotlinSerialization)
+    alias(libs.plugins.ktLint)
+    `maven-publish`
+}
+
+kotlin {
+    androidTarget {
+        publishAllLibraryVariants()
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        androidMain.dependencies {
+            implementation(libs.firebase.analytics.ktx)
+        }
+        commonMain.dependencies {
+            implementation(libs.kermit)
+            implementation(libs.kmp.launchpad.utils.domain)
+            implementation(libs.koin.core)
+            implementation(libs.kotlin.reflect)
+            implementation(libs.kotlin.stdlib.jdk7)
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.kotlinx.datetime)
+            implementation(libs.kotlinx.serialization.json)
+            implementation(libs.kvault)
+            implementation(libs.napier)
+        }
+    }
+}
+
+android {
+    namespace = "com.bottlerocketstudios.launchpad.utils"
+    compileSdk = libs.versions.compile.sdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.min.sdk.get().toInt()
+    }
+}
+
+ktlint {
+    verbose.set(true)
+    outputToConsole.set(true)
+    ignoreFailures.set(true)
+}
+
+group = "com.github.bottlerocketstudios"
+version = "${libs.versions.launchpad.compose.get()}-SNAPSHOT"
+
+publishing {
+    publications {
+        register<MavenPublication>("$name-release") {
+            artifactId = name.lowercase()
+            from(components["kotlin"])
+
+            pom {
+                description = "Compose components for Launchpad"
+
+                licenses {
+                    license {
+                        name = "The Apache License, Version 2.0"
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                    }
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/BottleRocketStudios/KMP-LaunchPad-Utils")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
